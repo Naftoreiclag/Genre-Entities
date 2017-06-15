fb8988a71ebe54c1088163cf5ad62043f7509fd4
Naftoreiclag <naftoreiclag@gmail.com>
2017-06-14 20:09:44 -0700

    Clean up main.cpp

--- src/pegr/main.cpp
+++ src/pegr/main.cpp
@@ -1,28 +1,20 @@
-#include <iostream>
-
 #include "pegr/gensys/Gensys.hpp"
 #include "pegr/script/Script.hpp"
 #include "pegr/logger/Logger.hpp"
 
 using namespace pegr;
 
-int test(lua_State* l) {
-    std::cout << "Hello from C++" << std::endl;
-    return 0;
-}
-
-int restricted(lua_State* l) {
-    std::cout << "You can't use this function" << std::endl;
-    return 0;
-}
-
-int main() {
+void setup_logger() {
     Logger::initialize();
+}
+
+void setup_scripting() {
     Script::initialize();
+}
+
+void setup_gensys() {
     Gensys::initialize();
-    
     const luaL_Reg api_safe[] = {
-        {"test", test},
         {"add_archetype", Gensys::li_add_archetype},
         {"edit_archetype", Gensys::li_edit_archetype},
         {"add_genre", Gensys::li_add_genre},
@@ -34,25 +26,34 @@ int main() {
         {nullptr, nullptr}
     };
     Script::multi_expose_c_functions(api_safe);
-    const luaL_Reg api_restricted[] = {
-        {"restricted", restricted},
-        
-        // End of the list
-        {nullptr, nullptr}
-    };
-    Script::multi_expose_c_functions(api_restricted, false);
-    {
-        Script::Regref_Guard sandbox(Script::new_sandbox());
-        Script::Regref_Guard init_fun(
-                Script::load_lua_function("init.lua", sandbox.regref()));
-        Script::Regref_Guard postinit_fun(
-                Script::load_lua_function("postinit.lua", sandbox.regref()));
-        
-        Script::run_function(init_fun.regref());
-        Gensys::compile();
-        Script::run_function(postinit_fun.regref());
-        
-    }
+}
+
+void setup() {
+    setup_logger();
+    setup_scripting();
+    setup_gensys();
+}
+
+void run() {
+    Script::Regref_Guard sandbox(Script::new_sandbox());
+    Script::Regref_Guard init_fun(
+            Script::load_lua_function("init.lua", sandbox.regref()));
+    Script::Regref_Guard postinit_fun(
+            Script::load_lua_function("postinit.lua", sandbox.regref()));
+    
+    Script::run_function(init_fun.regref());
+    Gensys::compile();
+    Script::run_function(postinit_fun.regref());
+    
+}
+
+void cleanup() {
     Script::cleanup();
+}
+
+int main() {
+    setup();
+    run();
+    cleanup();
     return 0;
 }
