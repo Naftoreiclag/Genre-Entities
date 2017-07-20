47870de9ba3c32c8884e25509e1cd30e46bbbe21
Naftoreiclag <naftoreiclag@gmail.com>
2017-07-20 15:12:18 -0700

    Improve test postinit debug output

--- run/postinit.lua
+++ run/postinit.lua
@@ -5,23 +5,23 @@ print('post init')
 -------------------------------------------------------------------------------
 
 cookie_at = pegr.find_archetype('cookie.at')
-print(cookie_at)
+print('cookie_at ' .. tostring(cookie_at))
 pizza_at = pegr.find_archetype('pizza.at')
-print(pizza_at)
+print('pizza_at ' .. tostring(pizza_at))
 ball_at = pegr.find_archetype('bowling_ball.at')
-print(ball_at)
+print('ball_at ' .. tostring(ball_at))
 
 local cookie1 = pegr.new_entity(cookie_at)
-print(cookie1)
+print('cookie1 ' .. tostring(cookie1))
 
 local cookie2 = pegr.new_entity(cookie_at)
-print(cookie2)
+print('cookie2 ' .. tostring(cookie2))
 
 local pizza1 = pegr.new_entity(pizza_at)
-print(pizza1)
+print('pizza1 ' .. tostring(pizza1))
 
 local bb1 = pegr.new_entity(ball_at)
-print(bb1)
+print('bb1 ' .. tostring(bb1))
 
 cookie1.position.x = 100
 cookie1.position.y = 100
