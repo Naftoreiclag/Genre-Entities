bfbc3006ea30aef4a0c110fed77712924024b17e
Naftoreiclag <naftoreiclag@gmail.com>
2017-07-22 23:53:55 -0700

    Simple sandbox test

---
+++ run/test/tests/0001_sandbox_test.lua
@@ -0,0 +1,23 @@
+--@Name Simple sandbox test
+
+local unsafe = {
+  'collectgarbage',
+  'dofile',
+  'dump',
+  'load',
+  'loadfile',
+  'loadstring',
+  'print',
+}
+
+local accessed = {}
+
+for _, name in ipairs(unsafe) do
+  if _G[name] ~= nil then 
+    table.insert(accessed, name)
+  end
+end
+
+if #accessed > 0 then
+  error('Accessed: ' .. table.concat(accessed, ', '))
+end
